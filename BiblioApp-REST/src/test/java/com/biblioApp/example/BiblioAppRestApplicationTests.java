b0e36fb8e457b17ef4033f6892a95e1d430f1457
Garrotxi <laroige@protonmail.com>
2021-10-18 23:08:56 +0200

    Creat el projecte Spring Boot

---
+++ BiblioApp-REST/src/test/java/com/biblioApp/example/BiblioAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioApp.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioAppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
