b34f2b8673cebc36c90f515eaa3c5c5461a6506e
Saul <slopezdiez@gmail.com>
2021-10-19 12:47:15 +0200

    Revert "Actualización con transiciones"
    This reverts commit a8bee7a83256f0c9e953148c7f1f2cc528674ff8.

--- BiblioApp-Android/BiblioApp/app/src/main/java/ioc/android/biblioapp/Splash_Activity.java
+++ BiblioApp-Android/BiblioApp/app/src/main/java/ioc/android/biblioapp/Splash_Activity.java
@@ -8,20 +8,12 @@ import android.util.Log;
 
 public class Splash_Activity extends AppCompatActivity {
 
-    private static final int TIEMPO_CARGA_SPLASH = 3000;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme_Splash);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Intent intent = new Intent(this, MainActivity.class);
-        // Dormimos el thread el tiempo definido para que se vea la pantalla de carga
-        try {
-            Thread.sleep(TIEMPO_CARGA_SPLASH);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+          Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
         finish();
 
