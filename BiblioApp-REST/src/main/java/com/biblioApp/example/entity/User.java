9a2be74dac4ae909e0f475e8b4b155bb3e3769bb
Cristian <cristianmp17@hotmail.com>
2021-10-20 12:49:07 +0200

    estructura principal
    Añadiendo paquetes principales.

--- BiblioApp-REST/src/main/java/com/biblioApp/example/entity/User.java
+++ BiblioApp-REST/src/main/java/com/biblioApp/example/entity/User.java
@@ -1,5 +1,42 @@
 package com.biblioApp.example.entity;
 
-public class User {
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+
+@Data
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+
 
 }
