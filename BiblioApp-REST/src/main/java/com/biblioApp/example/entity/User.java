97eace0740d834bc58acda63965095decffffaaa
Cristian <cristianmp17@hotmail.com>
2021-10-22 22:06:50 +0200

    funcionando auth
    auth

--- BiblioApp-REST/src/main/java/com/biblioApp/example/entity/User.java
+++ BiblioApp-REST/src/main/java/com/biblioApp/example/entity/User.java
@@ -1,6 +1,6 @@
 package com.biblioApp.example.entity;
 
-import lombok.Data;
+
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 
-@Data
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -51,15 +51,44 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
+    public Set<Role> getRoles() {
+        return roles;
+    }
 
-    @Column(name = "created_at")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdAt;
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 
-    @Column(name = "updated_at")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date updatedAt;
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
