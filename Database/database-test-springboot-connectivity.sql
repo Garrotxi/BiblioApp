30a01d460e8d5426b273d1c1b3f83ed1bf4d349e
Garrotxi <laroige@protonmail.com>
2021-11-10 22:05:08 +0100

    base de datos actualizada

--- Database/database-test-springboot-connectivity.sql
+++ Database/database-test-springboot-connectivity.sql
@@ -18,6 +18,59 @@ USE `biblioapp`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `llibre`
+--
+
+DROP TABLE IF EXISTS `llibre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `llibre` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `copies_disponibles` int(11) DEFAULT NULL,
+  `id_categoria` int(11) DEFAULT NULL,
+  `data_publicacio` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `titul` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `llibre`
+--
+
+LOCK TABLES `llibre` WRITE;
+/*!40000 ALTER TABLE `llibre` DISABLE KEYS */;
+/*!40000 ALTER TABLE `llibre` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `llibres`
+--
+
+DROP TABLE IF EXISTS `llibres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `llibres` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titul` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_categoria` int(11) DEFAULT NULL,
+  `data_publicacio` date DEFAULT NULL,
+  `copies_disponibles` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idllibres_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `llibres`
+--
+
+LOCK TABLES `llibres` WRITE;
+/*!40000 ALTER TABLE `llibres` DISABLE KEYS */;
+/*!40000 ALTER TABLE `llibres` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `roles`
 --
 
@@ -62,35 +115,8 @@ CREATE TABLE `user_roles` (
 
 LOCK TABLES `user_roles` WRITE;
 /*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
-INSERT INTO `user_roles` VALUES (1,1),(1,2),(2,1),(3,1),(3,2),(4,1),(5,1),(5,2),(6,1),(7,1),(8,1),(9,1),(10,1),(11,1),(12,1);
+INSERT INTO `user_roles` VALUES (1,1),(1,2),(2,1),(3,1),(3,2),(4,1),(5,1),(5,2),(6,1),(7,1),(8,1),(9,1),(10,1),(11,1),(12,1),(13,1),(14,1),(15,1),(16,1),(17,1),(18,1),(19,1),(20,1),(21,1),(22,1),(23,1),(24,1),(25,1);
 /*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'llu354s@mail.com',NULL,'$2a$10$jrU4bnJW4EqJhLKjcQKBMukqFaYxe7WNWkvE7xEfJj8NweyMRWYNu','34523456s'),(2,'asfgh@mail.com',NULL,'$2a$10$pYp6stFc73SpvPy8IIsAEeYavg4y2pw7q5vlMVxbKfovqSHDYjFLi','asdlkjfgg456s');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -102,12 +128,16 @@ DROP TABLE IF EXISTS `usuaris`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuaris` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
-  `nom` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
-  `contrasenya` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `nom_usuari` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `contrasenya` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `nom` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `telefon` int(11) DEFAULT NULL,
+  `data_registre` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` tinyint(4) DEFAULT '1' COMMENT 'STATUS - Camp per a definir el estat del usuari.\\n1 = Actiu\\n0 = Inactiu/Desactivat',
+  `cognoms` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,7 +146,7 @@ CREATE TABLE `usuaris` (
 
 LOCK TABLES `usuaris` WRITE;
 /*!40000 ALTER TABLE `usuaris` DISABLE KEYS */;
-INSERT INTO `usuaris` VALUES (9,'123456@mail.com',NULL,'$2a$10$JJDwP0KQJHtmhSOmO8IFkes2xR.TOvHiUytRMWhHdP0GDC5bEHyHe','234567'),(10,'32467dcgv@mail.com',NULL,'$2a$10$lF0WCkVWiKi/7/cq4NvMWuEEOR96EAvUb6yXvH/VPPLFqvlfzFhVS','sdfsdfsdf'),(11,'asdcvhrt@mail.com',NULL,'$2a$10$7YQjDLBwjwi6rGJWdjTjNeQ9WatRXjQwyYYmisHeGLWAiY.HBhv0W','asdyy54'),(12,'ewrzxchw@mail.com',NULL,'$2a$10$7lMQdO3XUgJMmtUHqZqEeeCMN0wBeJS0bTgMKQJXYoesn1HViHkuS','asdtbs');
+INSERT INTO `usuaris` VALUES (24,'lluisAdmin','$2a$10$ArqK6zQzGwCkfoQCf5XTUOvmt3nn4lINmg90KPom.G0H0taUl9IfK','lluis','lluis@mail.com',666666666,'2021-11-08 21:42:34',1,'roige'),(25,'lluisAdmin1','$2a$10$qaoPABuHlXcZ.OQ3SGAIOezlPXGUCtT2Yh.dehSU7WbsYlVUESg9u','lluis1','lluis1@mail.com',666666666,'2021-11-09 21:43:27',1,'roige');
 /*!40000 ALTER TABLE `usuaris` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -137,4 +167,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-08 20:41:03
+-- Dump completed on 2021-11-10 22:02:42
